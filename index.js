7d181303d41155d2ebce232bb9c2b4e198e4ef32
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-13 23:59:32 +0530

    feat: html elements selected in js file

---
+++ index.js
@@ -0,0 +1,3 @@
+const inputTextArea = document.querySelector("#input-text-area");
+const outputDiv = document.querySelector("#output-div");
+const translateButton = document.querySelector("#translate-btn");
