ce923018ff8a9b44cf6376ad32760920d1d5e2c3
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-14 00:08:22 +0530

    feat: translation query added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const inputTextArea = document.querySelector("#input-text-area");
 const outputDiv = document.querySelector("#output-div");
 const translateButton = document.querySelector("#translate-btn");
+const ferbApi = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+const translateQuery = (text) => {
+  return ferbApi + "?text=" + text;
+};
