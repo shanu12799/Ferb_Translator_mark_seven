62054f1e65675f6c03ffbb258173ac180c50d119
Sachin Gupta <sachinguptaa099@gmail.com>
2021-08-19 23:10:21 +0530

    first commit

--- Readme.md
+++ Readme.md
@@ -2,11 +2,3 @@
 
 A web app that converts English to Ferb Latin using a third-party API. Built using HTML, CSS, and Javascript.
 
-# Live Link
-
-https://translatethatminion.netlify.app
-
-# Salient features are
-
-- Used https://funtranslations.com/ferb-latin api
-- The app takes in input and translate the input into Ferb-latin.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const inputTextArea = document.querySelector("#input-text-area");
-const outputDiv = document.querySelector("#output-div");
-const translateButton = document.querySelector("#translate-btn");
-const ferbApi = "https://api.funtranslations.com/translate/ferb-latin.json";
+const inputTextArea = document.querySelector("#translate-input");
+const outputDiv = document.querySelector("#translate-output");
+const translateButton = document.querySelector("#translate-button");
+const ferbApi = "https://api.funtranslations.com/translate/pirate.json";
 
 const translateQuery = (text) => {
   return ferbApi + "?text=" + text;
